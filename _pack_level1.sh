838da114e8a283cba9a362993ab9dc2ac7ea7e57
althafvly <althafvly@gmail.com>
2022-02-08 09:46:36 +0530

    fix folder creation

--- _pack_level1.sh
+++ _pack_level1.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-if [ ! -d level1 ]
-then
+if [ ! -d level1 ]; then
     echo "Can't find level1 folder"
-fi
-
-count_file=`ls -1 level1/image.cfg 2>/dev/null | wc -l`
-if [ $count_file = 0 ]; then
+elif [ ! -d out ]; then
+    echo "Can't find out folder"
+    mkdir out
+elif [ "$(ls -1 level1/image.cfg 2>/dev/null | wc -l)" = 0 ]; then
     echo "Can't find image.cfg"
 else
     file_name=$(basename in/*.img)
-	bin/linux/AmlImagePack -r level1/image.cfg level1 out/$file_name
+	bin/linux/AmlImagePack -r level1/image.cfg level1 out/"$file_name"
 	echo "Done."
 fi
--- _unpack_level1.sh
+++ _unpack_level1.sh
@@ -2,8 +2,10 @@
 
 if [ -d level1 ]
 then
-    echo "Deleting existing $level_dir"
-    rm -rf level1
+    echo "Deleting existing level1"
+    rm -rf level1 && mkdir level1
+else
+    mkdir level1
 fi
 
 if [ ! -d in ]
@@ -12,10 +14,10 @@ then
     echo "Creating /in folder"
     mkdir in
 else
-	count_file=`ls -1 in/*.img 2>/dev/null | wc -l`
-	if [ $count_file = 0 ]; then
+	count_file=$(ls -1 in/*.img 2>/dev/null | wc -l)
+	if [ "$count_file" = 0 ]; then
 		echo "No files found in /in"
-	elif [ $count_file -ne 1 ]; then
+	elif [ "$count_file" -ne 1 ]; then
 		echo "Too many files found"
 	else
 		bin/linux/AmlImagePack -d in/*.img level1
