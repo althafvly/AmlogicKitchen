597e6680d29a5b3415de2082a380db2081e9755a
althafvly <althafvly@gmail.com>
2022-06-14 11:59:38 +0530

    Support for rockchip (windows)

---
+++ rkpack.bat
@@ -0,0 +1,179 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+setlocal EnableDelayedExpansion
+
+cls
+
+
+echo .....................
+echo Rockchip Kitchen
+echo .....................
+set /P level=Select level 1,2 or 3: 
+IF %level% == 1 GOTO level1
+IF %level% == 2 GOTO level2
+IF %level% == 3 GOTO level3
+ELSE echo Invalid option
+echo Unpack level 1 first
+pause
+exit
+
+:level1
+
+if exist level1 goto pass
+echo Unpack level 1 first
+pause
+exit
+
+:pass
+
+if exist out rmdir /q /s out
+md out
+
+set /p filename=< level1\projectname.txt
+bin\windows\afptool.exe -pack level1\ level1\Image\update.img
+bin\windows\rkImageMaker.exe -RK322H level1\Image\MiniLoaderAll.bin level1\Image\update.img "out\%filename%.img" -os_type:androidos
+
+pause
+exit
+
+:level2
+
+if exist level2 goto pass2
+echo Unpack level 2 first
+pause
+exit
+
+:pass2
+
+if not exist level1\Image\super.img (
+    FOR %%A IN (odm oem product vendor system system_ext odm_ext_a odm_ext_b) DO (
+        if exist level2\%%A\ (
+            set /p size=<"level2\config\%%A_size.txt"
+            bin\windows\make_ext4fs -J -L %%A -T -1 -S level2\config\%%A_file_contexts -C level2\config\%%A_fs_config -l !size! -a %%A level1\Image\%%A.img level2\%%A\
+        )
+    )
+)
+
+set metadata_size=65536
+set metadata_slot=3
+set supername=super
+
+if exist level1\Image\super.img (
+
+    FOR %%A IN (odm oem product vendor system system_ext system_a system_ext_a vendor_a product_a odm_a system_b system_ext_b vendor_b product_b odm_b) DO (
+        if exist level2\%%A\ (
+            bin\windows\du -sk level2\%%A | bin\windows\cut -f1 | bin\windows\gawk "{$1*=1024;$1=int($1*1.08)};echo $1"> level2\config\%%A_dir_size.txt
+            set /p size=<"level2\config\%%A_dir_size.txt"
+            IF !size! LSS 1048576 (
+                SET size=1048576
+                echo 1048576> level2\config\%%A_dir_size.txt
+            )
+            bin\windows\make_ext4fs -J -L %%A -T -1 -S level2\config\%%A_file_contexts -C level2\config\%%A_fs_config -l !size! -a %%A level2\%%A.img level2\%%A\
+            bin\windows\ext4\resize2fs.exe -M level2\%%A.img
+        )
+    )
+
+    set /p supersize=<"level2\config\super_size.txt"
+    bin\windows\du -cb level2/*.img | bin\windows\grep total | bin\windows\cut -f1>level2\superusage.txt
+    set /p superusage1=<"level2\superusage.txt"
+    set command=bin\windows\super\lpmake --metadata-size %metadata_size% --super-name %supername% --metadata-slots %metadata_slot% --device %supername%:!supersize! --group amlogic_dynamic_partitions_a:!superusage1!
+
+    FOR %%A IN (system_a system_ext_a vendor_a product_a odm_a) DO (
+        if exist level2\%%A.img (
+            bin\windows\du -skb level2\%%A.img | bin\windows\cut -f1> level2\%%A_size.txt
+            set /p size=<"level2\%%A_size.txt"
+            echo %%A | bin\windows\sed "s/.\{3\}$//">level2\%%A.txt
+            set /p name=<"level2\%%A.txt"
+            if !size! GTR 0 (
+                set command=!command! --partition %%A:readonly:!size!:amlogic_dynamic_partitions_a --image %%A=level2\%%A.img
+            )
+            del level2\*.txt
+        )
+    )
+    set /a superusage2=!supersize!-!superusage1!
+    set command=!command! --group amlogic_dynamic_partitions_b:!superusage2!
+
+    FOR %%A IN (system_b system_ext_b vendor_b product_b odm_b) DO (
+        if exist level2\%%A.img (
+            bin\windows\du -skb level2\%%A.img | bin\windows\cut -f1> level2\%%A_size.txt
+            set /p size=<"level2\%%A_size.txt"
+            if !size! EQU 0 (
+                set command=!command! --partition %%A:readonly:!size!:amlogic_dynamic_partitions_b
+            )
+            del level2\*.txt
+        )
+    )
+
+    if !superusage1! GEQ !supersize! (
+        echo Unable to create super image, recreated images are too big.
+        echo Cleanup some files before retrying
+        echo Needed space: !superusage1!
+        echo Available maximum space: !supersize!
+        pause
+        exit
+    )
+
+    set command=!command! --virtual-ab --sparse --output level1\Image\super.img
+    !command!
+)
+
+echo Done.
+pause
+exit
+
+:level3
+
+if exist level3 goto pass3
+echo Unpack level 3 first
+pause
+exit
+
+:pass3
+
+FOR %%A IN (recovery boot recovery_a boot_a) DO (
+    if exist level3\%%A\ (
+        call bin\windows\aik\cleanup.bat
+        if exist level3\%%A\ramdisk\ (
+            move level3\%%A\ramdisk bin\windows\aik\
+        )
+        move level3\%%A\split_img bin\windows\aik\
+        call bin\windows\aik\repackimg.bat
+        move bin\windows\aik\image-new.img bin\windows\aik\%%A.img
+        call bin\windows\aik\unpackimg.bat bin\windows\aik\%%A.img
+        if exist bin\windows\aik\ramdisk\ (
+            move bin\windows\aik\ramdisk level3\%%A\
+        )
+        move bin\windows\aik\split_img  level3\%%A\
+        move bin\windows\aik\%%A.img level1\Image\%%A.img
+        call bin\windows\aik\cleanup.bat
+    )
+)
+
+echo Done.
+pause
+exit
---
+++ rkunpack.bat
@@ -0,0 +1,171 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+setlocal EnableDelayedExpansion
+
+cls
+
+
+echo .....................
+echo Rockchip Kitchen
+echo .....................
+set /P level=Select level 1,2 or 3: 
+IF %level% == 1 GOTO level1
+IF %level% == 2 GOTO level2
+IF %level% == 3 GOTO level3
+echo Unpack level 1 first
+pause
+exit
+
+:level1
+
+if exist level1 rmdir /q /s level1
+md level1
+
+echo .....................
+echo Rockchip Kitchen
+echo .....................
+echo Files in input dir (*.img)
+if not exist in\*.img (
+  echo Can't find images
+  if not exist in\ (
+    mkdir in
+  )
+  pause
+  exit 0
+)
+
+SET /A COUNT=0
+for %%a in (in\*.img) do set /a count += 1 && echo !count! - %%~na
+echo .....................
+set /P projectname=Enter a file name :
+echo %projectname%> level1\projectname.txt
+
+if not exist in\%projectname%.img (
+echo Can't find the file
+pause
+exit 0
+)
+
+set /p filename=< level1\projectname.txt
+
+bin\windows\rkImageMaker.exe -unpack "in\%filename%.img" level1
+bin\windows\afptool.exe -unpack level1\firmware.img level1
+
+pause
+exit
+
+
+:level2
+
+python --version 2>NUL
+if errorlevel 1 (
+echo Error^: Python not installed
+pause
+exit
+)
+
+if exist level1 goto pass2
+echo Unpack level 1 first
+pause
+exit
+
+:pass2
+
+if exist level2 rmdir /q /s level2
+md level2\config\
+
+FOR %%A IN (odm oem product vendor system system_ext odm_ext_a odm_ext_b) DO (
+if exist level1\Image\%%A.img (
+    python bin\common\imgextractor.py "level2\%%A.img" "level2"
+    if exist level1\Image\%%A.raw.img (
+        del level1\Image\%%A.raw.img
+    )
+    if exist level2\config\%%A_file_contexts (
+        bin\windows\sed -n "G; s/\n/&&/; /^\([ -~]*\n\).*\n\1/d; s/\n//; h; P" level2\config\%%A_file_contexts > level2\config\%%A_sorted_file_contexts
+        move level2\config\%%A_sorted_file_contexts level2\config\%%A_file_contexts
+    )
+)
+)
+
+if exist level1\Image\super.img (
+    bin\windows\simg2img level1\Image\super.img level2\super.img
+    bin\windows\du -sb level2\super.img | bin\windows\cut -f1> level2\config\super_size.txt
+    bin\windows\super\lpunpack -slot=0 level2\super.img level2\
+    del level2\super.img
+
+    FOR %%A IN (odm oem product vendor system system_ext system_a system_ext_a vendor_a product_a odm_a system_b system_ext_b vendor_b product_b odm_b) DO (
+    if exist level2\%%A.img (
+        call :setsize level2\%%A.img
+        if !size! GTR 1024 (
+            python bin\common\imgextractor.py "level2\%%A.img" "level2"
+        )
+        if exist level2\config\%%A_file_contexts (
+            bin\windows\sed -n "G; s/\n/&&/; /^\([ -~]*\n\).*\n\1/d; s/\n//; h; P" level2\config\%%A_file_contexts > level2\config\%%A_sorted_file_contexts
+            move level2\config\%%A_sorted_file_contexts level2\config\%%A_file_contexts
+        )
+    )
+    )
+)
+
+:setsize
+set size=%~z1
+goto :eof
+
+echo Done.
+pause
+exit
+
+:level3
+
+if exist level1 goto pass3
+echo Unpack level 1 first
+pause
+exit
+
+:pass3
+
+if exist level3 rmdir /q /s level3
+md level3 level3\boot level3\boot_a level3\recovery level3\recovery_a
+
+FOR %%A IN (recovery boot recovery_a boot_a) DO (
+    if exist level1\Image\%%A.img (
+        copy level1\Image\%%A.img bin\windows\aik\%%A.img
+        call bin\windows\aik\unpackimg.bat bin\windows\aik\%%A.img
+        if exist bin\windows\aik\ramdisk\ (
+            move bin\windows\aik\ramdisk level3\%%A\
+        )
+        move bin\windows\aik\split_img level3\%%A\
+        del bin\windows\aik\%%A.img
+    )
+)
+
+ROBOCOPY level3 level3 /S /MOVE /NFL /NDL /NJH /NJS /nc /ns /np
+
+echo Done.
+pause
+exit
